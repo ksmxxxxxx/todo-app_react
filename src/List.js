ccad2daa7bb40569b24403cb98a54c58b0e85bc3
ksm240 <kankichi69@gmail.com>
2020-01-30 00:44:56 +0900

    Add class when change class

--- src/List.js
+++ src/List.js
@@ -8,8 +8,9 @@ export class List extends React.Component {
   render() {
       const listItem = this.props.todolist.map((elem, i) => {
         const doneButtonLable = elem.done ? 'incompleted' : 'complete';
+        const doneClassLabel = elem.done ? 'complete' : 'incomplete';
         return <li key={i}>
-          {elem.title}
+          <span className={ doneClassLabel }>{elem.title}</span>
             <button onClick={ () => this.props.updateStatus(elem.index) }>{ doneButtonLable }</button>
             <button onClick={ () => this.props.removeTodo(elem.index) }>delete</button>
           </li>
